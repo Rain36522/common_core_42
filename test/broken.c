a7c4a3385aad4fb617b8072223b5a909c3d2a748
Paul <rainpaul365@gmail.com>
2024-05-20 15:36:16 +0200

    add code

--- test/broken.c
+++ test/broken.c
@@ -1,80 +1,78 @@
 #include<unistd.h>
 #include<stdlib.h>
-#include<string.h>
 #include<stdio.h>
+#include<string.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 
-
-int     clientsIds[1024];
-char    recv_msg[6000];
 char    send_msg[6000];
+char    recv_msg[6000];
+int     clients[1024];
+int     maxfd;
 fd_set  read_fd;
 fd_set  write_fd;
 fd_set  current;
-int     maxfd;
 
-void    error(void)
+
+void    error(int errorCode)
 {
-    write(2, "Fatal error\n", 12);
+    if (errorCode)
+        write(2, "Wrong arg number!\n", 18);
+    else
+        write(2, "Fatal error\n", 12);
     exit(1);
 }
 
-void    error_arg(void)
+void    broadcast(int exceptfd)
 {
-    write(2, "Wrong arguments number!\n", 24);
-    exit(1);
-}
-
-
-void    broadcast(int   fd_except)
-{
-    for (int fd = 0; fd <= maxfd; fd++)
+    for (int fd = 0; fd < maxfd; fd ++)
     {
-        if (FD_ISSET(fd, &write_fd) && fd != fd_except)
+        if (FD_ISSET(fd, &write_fd) && fd != exceptfd)
         {
             if (send(fd, send_msg, strlen(send_msg), 0) < 0)
-                error();
+                error(0);
         }
     }
+    bzero(send_msg, sizeof(send_msg));
 }
-
 
 
 int main(int argc, char **argv)
-{   
+{
     if (argc != 2)
-        error_arg();
+        error(1);
+    
+    char                    *ptr;
+    struct sockaddr_in      servaddr;
+    socklen_t               len;
 
-    char                *ptr;
-    struct sockaddr_in  servaddr;
-    socklen_t           len;
     int servfd = socket(2, 1, 0);
     if (servfd < 0)
-        error();
+        error(0);
     maxfd = servfd;
     FD_ZERO(&current);
     FD_SET(servfd, &current);
     bzero(&servaddr, sizeof(servaddr));
-    bzero(clientsIds, sizeof(clientsIds));
+    bzero(clients, sizeof(clients));
     servaddr.sin_family = 2;
     servaddr.sin_addr.s_addr = htonl(0);
     servaddr.sin_port = htons(atoi(argv[1]));
 
     if (bind(servfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 || listen(servfd, 100) < 0)
-        error();
+        error(0);
     
-    while (1)
+    while(1)
     {
         read_fd = current;
         write_fd = current;
+
         if (select(maxfd + 1, &read_fd, &write_fd, 0, 0) < 0)
             continue;
-        for (int fd = 0; fd <= maxfd; fd++)
+        for (int fd = 0; fd < maxfd; fd ++)
         {
-            if (FD_ISSET(fd, &read_fd)) // read_fd
+            if (FD_ISSET(fd, &read_fd))
             {
-                if (fd == servfd)
+                if (fd ==servfd)
                 {
                     int clientfd = accept(fd, (struct sockaddr *)&servaddr, &len);
                     if (clientfd < 0)
@@ -82,41 +80,30 @@ int main(int argc, char **argv)
                     if (maxfd < clientfd)
                         maxfd = clientfd;
                     FD_SET(clientfd, &current);
-                    clientsIds[clientfd] = maxfd ++;
-                    sprintf(send_msg, "Client %d join.", clientsIds[clientfd]);
+                    clients[clientfd] = maxfd ++;
+                    sprintf(send_msg, "Client %d join.\n", clients[clientfd]);
                     broadcast(clientfd);
-                    bzero(send_msg, strlen(send_msg));
-                }
-            }
-            else
-            {
-                int data = recv(fd, recv_msg, sizeof(recv_msg), 0);
-                if (data <= 0)
-                {
-                    FD_CLR(fd, &current);
-                    close(fd);
-                    sprintf(send_msg, "Client %d just left.", clientsIds[fd]);
-                    broadcast(fd);
-                    bzero(send_msg, sizeof(send_msg));
                 }
                 else
                 {
-                    ptr = recv_msg;
-                    for (int i = 0, j = 0; j < data; i ++, j ++)
+                    int data = recv(fd, recv_msg, sizeof(recv_msg), 0);
+                    if (data <= 0)
                     {
-                        if (ptr[i] == '\n')
+                        FD_CLR(fd, &current);
+                        close(fd);
+                        sprintf(send_msg, "Client %d left.", clients[fd]);
+                        broadcast(fd);
+                    }
+                    else
+                    {
+                        ptr = recv_msg;
+                        for (int i = 0, j = 0; j < data; j ++, i ++)
                         {
-                            ptr[i] = '\0';
-                            sprintf(send_msg, "Client %d send : %s\n", clientsIds[fd], ptr);
-                            broadcast(fd);
-                            bzero(send_msg, i);
-                            ptr += i + 1;
-                            i = -1;
+                            if (i)
                         }
                     }
                 }
             }
-            break;
         }
     }
 }
